994d706fc74d8aaa9275dffcd4fb34d6300209d9
Shijoy Bharath <shijoybharath8@gmail.com>
2024-12-26 19:31:29 +0530

    Adding class

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 def main():
-    print("Hello from 3d-modeller-python!")
+    print("Running Scene...")
+
+class Viewer(object):
+    def __init__(self):
+        '''Initialize viewer'''
+        self.init_interface()
+        self.init_opengl()
+        self.init_scene()
+        self.init_interaction()
+        init_primitives()
+
+    def init_interface(self):
+        '''Initialize window and register render function'''
+        glutInit()
 
 if __name__ == "__main__":
     main()
